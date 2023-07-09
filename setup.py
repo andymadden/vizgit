615c6bb130d54f46674247b0f4fc2e8f42e246a3
amadden <andrewsmadden@gmail.com>
2023-07-09 12:25:42 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+### ~~~This is a setup.py~~~ ###
+from distutils.core import setup
+
+setup(name="VizGit",
+      version='1.0',
+      description='Visual Git manager for VIM written in python',
+      author='Andrew Madden',
+      author_email='andrewsmadden@gmail.com',
+      url='TBD',
+      packages=['vizgit']
+      )
---
+++ vizgit/methods.py
@@ -0,0 +1,3 @@
+
+def print_something_specific() -> None:
+    print("Something Specific")
